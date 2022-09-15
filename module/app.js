81960f801b601271760a117568a53ad2b3a41ac8
piao11piao <piao11piao@gmail.com>
2022-09-15 20:33:47 +0200

    modified:   module/app.js 	new file:   src/model/resModel.js

--- module/app.js
+++ module/app.js
@@ -1,9 +1,12 @@
 const handleBlogRouter = require('../src/router/blog');
 const handleUserRouter = require('../src/router/user');
-
+const url = require('url');
 const serverHandle = (req, res) => {
     res.setHeader('Content-type','application/json');
-
+    const q = url.parse(req.url, true);
+    var path=q.pathname;
+    var method = req.method;
+    var searchquery = q.searchquery;
     const blogData = handleBlogRouter(req, res)
     if(blogData){
         res.end(JSON.stringify(blogData));
